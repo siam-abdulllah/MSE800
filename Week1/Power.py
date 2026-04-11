19017cb75916c25e528c937ea58edb161517a46e
SiamAbdulllah <syed.abdullah@selisegroup.com>
2026-04-12 02:57:21 +0600

    remove other file

--- Week1/Power.py
+++
@@ -1,9 +0,0 @@
-def CalculatePower(a, b):
-    return a ** b
-
-if __name__ == "__main__":
-    a = float(input("enter base (a): "))
-    b = float(input("bnter exponent (b): "))
-    
-    result = CalculatePower(a, b)
-    print(f"{a} taken to the power of {b} is {result}")
