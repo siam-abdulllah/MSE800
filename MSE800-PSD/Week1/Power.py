f75b6d0ff08feca0d50642b614505b6dafad0088
SiamAbdulllah <syed.abdullah@selisegroup.com>
2026-04-12 02:55:33 +0600

    Create MSE800-PSD

---
+++ MSE800-PSD/Week1/Power.py
@@ -0,0 +1,9 @@
+def CalculatePower(a, b):
+    return a ** b
+
+if __name__ == "__main__":
+    a = float(input("enter base (a): "))
+    b = float(input("bnter exponent (b): "))
+    
+    result = CalculatePower(a, b)
+    print(f"{a} taken to the power of {b} is {result}")
---
+++ MSE800-PSD/Week1/README.md
@@ -0,0 +1,11 @@
+Week 1 - Power Calculator
+Description
+This project calculates the power of a number (a^b).
+
+How to Run
+python power.py
+
+Example
+2^3 = 8
+
+Screenshot
